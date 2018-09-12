73f386f78d826312bc5d0c01ab43c86920cf255d
Yuri Kim <yuri@Yuris-MacBook-Pro.local>
2018-09-13 00:30:00 +0900

    added recipient to comment

--- db/migrate/20180829130052_create_comments.rb
+++ db/migrate/20180829130052_create_comments.rb
@@ -2,6 +2,7 @@ class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
       t.belongs_to :author,            null: false, foreign_key: { to_table: :users }
+      t.belongs_to :recipient,         null: false, foreign_key: { to_table: :users }
       t.belongs_to :answer,            null: false
       t.text       :content,           null: false
 
