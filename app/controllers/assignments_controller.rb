507d6b0733d89bdf886ff622a2a1aed43a098153
leesoojeong <sjlee739@gmail.com>
2018-09-19 02:34:26 +0900

    add message for checking assignment

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -2,9 +2,10 @@ class AssignmentsController < ApplicationController
     def create
         question_id = params[:question_id]
         user_id = params[:user_id]
+        assigned_user = User.find(user_id)
 
         i=0
-        User.find(user_id).answers.each do |answer|
+        assigned_user.answers.each do |answer|
             puts answer.question.id
             if answer.question.id.to_i == question_id.to_i
                 break
@@ -12,12 +13,17 @@ class AssignmentsController < ApplicationController
             i += 1
         end
 
-        if i == User.find(user_id).answers.count
+        if i == assigned_user.answers.count
             Assignment.create(question_id: question_id, assigner_id: current_user.id, assignee_id: user_id)
+            flash[:success] = "#{assigned_user.email}님을 assign하셨습니다."
+            # render json: {status: 'success', message: "#{assigned_user.email}님을 assign하셨습니다."}
         else
+            # render json: {status: 'failure', message: "#{assigned_user.email}님은 이미 질문에 답하셨습니다."}
+            flash[:error] = "#{assigned_user.email}님은 이미 질문에 답하셨습니다."
         end
 
         redirect_to root_url
+
     end
 
     def delete
@@ -26,6 +32,8 @@ class AssignmentsController < ApplicationController
         assignment.destroy
         noti = Notification.find_by(target: assignment)
         noti.destroy
+
+        flash[:success] = "#{User.find(assignment.assignee_id).email}님을 de-assign하셨습니다."
 
         redirect_to root_url
     end
