98b883581a60713249c75fc6b248dcf22b992acd
ssm0318 <kleenex0318@gmail.com>
2018-09-23 03:11:44 +0900

    temp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user!, only: [:friends]
+
     def friends
         #TODO : change User.find(1) to current_user after sign_in
-        @friends = User.find(3).friends
+        @friends = current_user.friends
     end
 end
