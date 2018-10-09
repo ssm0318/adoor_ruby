129d6361b61f8acf18bafbf81c531d2e57f987aa
leegakyeong <alaska7132@naver.com>
2018-10-09 18:11:23 +0900

    뭘 고친 거지 이건..?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,6 @@ class UsersController < ApplicationController
         puts user_email
         # redirect_to action: 'recover_password_confirm'
         render 'recover_password_confirm'
+        # 아 redirect_to 써야겠구나.... 새로고침하면 도로 recover_password 페이지로 돌아감 ㅠ
     end
 end
