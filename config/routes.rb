6d242a69f27dc70eb24b30f8f67cb7b1852ebd84
Yuri Kim <yuri@Yuris-MacBook-Pro.local>
2018-10-09 22:41:39 +0900

    answers, highlights, stars routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'questions#index'
+
+  resources :answers
+
+  resources :highlights
+  resources :stars
+  
+  # user answers, highlights, stars
+  get '/:id' => 'answers#user_answers', as: :user_answers
+  get '/:id/highlights' => 'highlights#user_highlights', as: :user_highlights
+  get '/:id/stars' => 'stars#user_stars', as: :user_stars
+
 end
