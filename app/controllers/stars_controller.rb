2d9d3df068514eb10f46af688491f3d7242fe3df
Yuri <njs03332@gmail.com>
2018-10-10 02:52:11 +0900

    starring questions implemented

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -2,9 +2,7 @@ class StarsController < ApplicationController
     before_action :authenticate_user!
 
     def create
-        # TODO:
-        # target이 answer일 때만 만들었음! question일 때까지 포괄하려면 코드를 추가해야 한다
-        Star.create(user_id: current_user.id, target: Answer.find(params[:target_id]))
+        Star.create(user_id: current_user.id, target_id: params[:target_id], target_type: params[:target_type])
 
         # fallback_location은 그냥 임시
         redirect_back fallback_location: user_stars_path(current_user.id)
@@ -16,9 +14,7 @@ class StarsController < ApplicationController
     end
 
     def destroy
-        # TODO:
-        # 이것도 target이 answer일 때만 있음! question까지 포괄하고만 싶다
-        star = Star.where(target_type: "Answer", target_id: params[:id], user_id: current_user.id)
+        star = Star.where(target_type: params[:target_type], target_id: params[:id], user_id: current_user.id)
         star.destroy_all
 
         # fallback_location은 그냥 임시
