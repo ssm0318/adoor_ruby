cc38d551a98cc779f8a2dccdf1f73a1a52b7219e
Yuri <njs03332@gmail.com>
2018-10-09 22:52:40 +0900

    highlights controller and views

---
+++ app/controllers/highlights_controller.rb
@@ -0,0 +1,36 @@
+class HighlightsController < ApplicationController
+    def user_highlights
+        @user = User.find(params[:id])
+        @highlights = @user.highlights
+    end
+
+    def destroy
+        highlight = Highlight.find(params[:id])
+        highlight.destroy
+
+        # FIXME: current_user.id로 바꾸자
+        redirect_to user_highlights_path(1)
+    end
+
+    # edit은 없어도 되겠지요..? 지우거나 새로 형광펜 하면 되니까
+    # def edit
+    #     set_highlight
+    # end
+
+    # def update
+    #     set_highlight
+    #     @highlight.update(highlight_params)
+    #     redirect_to @highlight
+    # end
+
+    private
+        # use highlight_params in create
+        def highlight_params
+            params.require(:highlight).permit(:content, :author_id, :answer_id)
+        end
+
+        def set_highlight
+            @highlight = Highlight.find(params[:id])
+        end
+    
+end
