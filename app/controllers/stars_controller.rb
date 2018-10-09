ffe03e59f4dbe52456cd69c8a4223f0df6a478a9
Yuri <njs03332@gmail.com>
2018-10-10 02:16:32 +0900

    create, destroy star controller and views

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -2,7 +2,12 @@ class StarsController < ApplicationController
     before_action :authenticate_user!
 
     def create
-        #@star = Star.create()
+        # TODO:
+        # target이 answer일 때만 만들었음! question일 때까지 포괄하려면 코드를 추가해야 한다
+        Star.create(user_id: current_user.id, target: Answer.find(params[:target_id]))
+
+        # fallback_location은 그냥 임시
+        redirect_back fallback_location: user_stars_path(current_user.id)
     end
 
     def user_stars
@@ -11,9 +16,12 @@ class StarsController < ApplicationController
     end
 
     def destroy
-        star = Star.find(params[:id])
-        star.destroy
-        
-        redirect_to user_stars_path(current_user.id)
+        # TODO:
+        # 이것도 target이 answer일 때만 있음! question까지 포괄하고만 싶다
+        star = Star.where(target_type: "Answer", target_id: params[:id], user_id: current_user.id)
+        star.destroy_all
+
+        # fallback_location은 그냥 임시
+        redirect_back fallback_location: user_stars_path(current_user.id)
     end
 end
