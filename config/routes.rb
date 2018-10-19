305d889d59f2b4d97657bf6d50f190ba71d8e91b
leegakyeong <alaska7132@naver.com>
2018-10-19 15:13:47 +0900

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'questions#index'
+  # root 'questions#index'
+  get '/feeds' => 'questions#index'
   get '/questions/:id' => 'questions#question_feed'
   # 이거 나중에 id 말고 content로 하는 게 더 보기 좋을 듯..! 
   get '/recover_password' => 'users#recover_password'
