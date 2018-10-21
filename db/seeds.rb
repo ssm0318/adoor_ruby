7d0a855b16a51998beecfd129b22ab328b7ce136
Yuri <njs03332@gmail.com>
2018-10-21 13:00:47 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -81,10 +81,7 @@ Highlight.create(user_id: 3, answer_id: 6, content: "초를 꽂지 말자고")
 Highlight.create(user_id: 1, answer_id: 4, content: "높은 곳에서")
 Highlight.create(user_id: 3, answer_id: 14, content: "저절로, 습관대로 행동하고 생각하고 말하게 되는 순간이 무섭다.")
 Highlight.create(user_id: 3, answer_id: 12, content: "충분히 쉬고 시작할 수 있어서 좋다.")
-<<<<<<< HEAD
 Highlight.create(user_id: 3, answer_id: 15, content: "세상에 저절로 되는 일은 없다.")
-=======
->>>>>>> 2a637152543cd7d0fa34787b112f08e47856e907
 
 Comment.create(author_id: 2, recipient_id: 2, answer_id: 1, content: "oh really?")
 Comment.create(author_id: 3, recipient_id: 3, answer_id: 3, content: "awesome!")
