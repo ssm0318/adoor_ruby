2e88e6b677210211dd8e0af1d1abdeda893b406d
leesoojeong <sjlee739@gmail.com>
2018-11-20 12:15:55 +0900

    finish noti

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -17,9 +17,15 @@ class NotificationsController < ApplicationController
         if noti.target_type == 'Friendship'
             redirect_to "/userpage/#{noti.actor_id}"
         elsif noti.target_type == 'Assignment'
-            redirect_to "/answers/new/question/#{noti.target_id}"
-        else
+            redirect_to "/answers/new/question/#{Assignment.find(noti.target_id).question_id}"
+        elsif noti.target_type == 'Answer'
             redirect_to "/answers/#{noti.target_id}"
+        elsif noti.target_type == 'Comment'
+            redirect_to "/answers/#{Comment.find(noti.target_id).answer_id}"
+        elsif noti.target_type == 'Highlight'
+            redirect_to "/answers/#{Highlight.find(noti.target_id).answer_id}"
+        elsif noti.target_type == 'Stars'
+            redirect_to "/answers/#{Star.find(noti.target_id).answer_id}"
         end
 
     end
