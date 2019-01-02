e071f5d579143a7c645483637a6b442fe88bebaa
leesoojeong <sjlee739@gmail.com>
2019-01-02 17:18:07 +0900

    remove unnecessary

--- app/controllers/api/v1/base_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::BaseController < ApplicationController
-    protect_from_forgery with: :null_session
-
-    before_action :destroy_session
-
-    def destroy_session
-        request.session_options[:skip] = true
-    end
-end
--- app/controllers/api/v1/questions_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::QuestionsController < Api::V1::BaseController
-    def show
-      question = Question.find(params[:id])
-      render(json: Api::V1::QuestionSerializer.new(question).to_json)
-    end
-  end
