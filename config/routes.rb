7b4d832bd265474a6b968202b2b7fd97c55dc2cb
ssm0318 <kleenex0318@gmail.com>
2019-01-05 19:37:42 +0900

    resolve conflict

--- config/routes.rb
+++ config/routes.rb
@@ -14,15 +14,9 @@ Rails.application.routes.draw do
   get '/recover_password' => 'users#recover_password'
   post '/recover_password' => 'users#send_temporary_password'
 
-<<<<<<< HEAD
-  resources :answers, except: :new
-  get '/answers/new/question/:id' => 'answers#new', as: :new_answer
-
-=======
   resources :answers, except: [:new, :edit]
   get '/answers/new/question/:id' => 'answers#new', as: :new_answer
   get '/answers/edit/question/:id' => 'answers#edit', as: :edit_answer
->>>>>>> master
 
   resources :highlights
   resources :stars
