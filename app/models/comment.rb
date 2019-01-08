51d5b6a4ea8eec1ed3a076f779507c658f5ea89b
Yuri <njs03332@gmail.com>
2019-01-08 11:24:26 +0900

    comment noti fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,7 +8,22 @@ class Comment < ApplicationRecord
     private
 
     def create_notifications
-        Notification.create(recipient: self.target.author, actor: self.author, target: self)
+        # 익명 댓글인 경우
+        if self.recipient.nil?
+            if self.author != self.answer.author
+                # 익명의 사람이 댓글을 단 경우
+                Notification.create(recipient: self.answer.author, actor: self.author, target: self, action: 'anonymous_comment')
+            end
+        # 친구 댓글인 경우
+        else
+            # 글쓴이가 다른사용자의 댓글에 대해 대댓글을 단 경우
+            if self.author == self.answer.author && self.recipient != self.answer.author
+                Notification.create(recipient: self.recipient, actor: self.author, target: self, action: 'recomment')
+            # 다른사용자가 글쓴이의 답변에 댓글을 단 경우
+            elsif self.author != self.answer.author
+                Notification.create(recipient: self.answer.author, actor: self.author, target: self, action: 'comment')
+            end    
+        end
     end
 end
  
