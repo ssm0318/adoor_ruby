25a928e830ae6d3cef33148774104c6190c169e0
leesoojeong <sjlee739@gmail.com>
2019-01-11 04:47:08 +0900

    delete ajax bug fix

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -86,15 +86,6 @@ class AnswersController < ApplicationController
         id = params[:recipient_id]
         if id == '0'
             c = Comment.create(content: params[:content], author_id: current_user.id, target: Answer.find(params[:id]))
-            # render json: {
-            #     comment_id: c.id,
-            #     imageurl: current_user.image.url,
-            #     path: user_answers_path(current_user.id),
-            #     username: current_user.username,
-            #     content: params[:content],
-            #     like_url: likes_path(target_id: c.id, target_type: 'Comment'), 
-            #     like_changed_url: like_path(c.id, target_type: 'Comment'),
-            # }
 
             render json: {
                 content: c.content,
@@ -105,14 +96,7 @@ class AnswersController < ApplicationController
             }
         else
             c = Comment.create(content: params[:content], author_id: current_user.id, recipient_id: params[:recipient_id], target: Answer.find(params[:id]))
-            # render json: {
-            #     imageurl: current_user.image.url,
-            #     path: user_answers_path(current_user.id),
-            #     username: current_user.username,
-            #     content: params[:content],
-            #     like_url: likes_path(target_id: c.id, target_type: 'Comment'), 
-            #     like_changed_url: like_path(c.id, target_type: 'Comment'),
-            # }
+
             render json: {
                 content: c.content,
                 created_at: c.created_at,
@@ -132,15 +116,6 @@ class AnswersController < ApplicationController
         answer_author_id = r.comment.target.author_id
         
         # redirect_back fallback_location: user_answers_path(answer_author_id)
-
-        # render json: {
-        #     imageurl: current_user.image.url,
-        #     path: user_answers_path(current_user.id),
-        #     username: current_user.username,
-        #     content: params[:content],
-            # like_url: likes_path(target_id: r.id, target_type: 'Reply'), 
-            # like_changed_url: like_path(r.id, target_type: 'Reply'),
-        # }
 
         render json: {
             content: r.content,
