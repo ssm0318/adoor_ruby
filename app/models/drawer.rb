5738f0de83c1bba663a7532a5035074827472011
Yuri <njs03332@gmail.com>
2019-01-11 18:11:01 +0900

    noti ~외 몇명이

--- app/models/drawer.rb
+++ app/models/drawer.rb
@@ -7,11 +7,13 @@ class Drawer < ApplicationRecord
     private 
 
     def create_notifications
-        #noti_hash = {recipient: self.target.author, origin: self.target}
-        #if Notification.where(noti_hash).unread.empty?
-            Notification.create(recipient: self.target.author, actor: self.user, target: self, origin: self.target)
-        #else
-            #Notification.where(noti_hash).first.target = self
-        #end
+        if self.target.author != self.user
+            noti_hash = {recipient: self.target.author, origin: self.target}
+            if Notification.where(noti_hash).unread.empty?
+                Notification.create(recipient: self.target.author, actor: self.user, target: self, origin: self.target)
+            else
+                Notification.where(noti_hash).first.target = self
+            end
+        end
     end
 end
