0a403d07cb8562f94a02d5545f7246bd331fea2c
ssm0318 <kleenex0318@gmail.com>
2019-01-12 04:19:56 +0900

    temp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,10 @@ class ApplicationController < ActionController::Base
   # def after_sign_in_path_for(resource)
   #   current_user_path
   # end
+
+  def intro
+    render 'intro'
+  end
  
   protected
  
