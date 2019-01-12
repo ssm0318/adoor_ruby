681085942a7a74033159b2171d74a1b180398cff
ssm0318 <kleenex0318@gmail.com>
2019-01-12 21:20:57 +0900

    temp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # in order to override the default behavior of devise 
-  # def after_sign_in_path_for(resource)
-  #   current_user_path
-  # end
-
   def intro
     render './intro'
   end
