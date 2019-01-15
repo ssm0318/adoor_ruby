1d08ea4b4717e5dfe51c0380a256763ba288bf72
ssm0318 <kleenex0318@gmail.com>
2019-01-16 02:27:45 +0900

    like noti error fix

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -14,9 +14,6 @@ class NotificationsController < ApplicationController
             if noti.read_at == nil
                 noti.read_at = DateTime.now()
                 noti.save(touch: false)  # updated_at을 update하지 않기 위해!!
-                puts '=================================================='
-                puts noti.errors.full_messages
-                puts '=================================================='
             end
 
             origin_id = noti.origin_id
