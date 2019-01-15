4104cd151f0a8cb90eeee9a552004e257033e75b
ssm0318 <kleenex0318@gmail.com>
2019-01-16 01:50:38 +0900

    indentation

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -65,8 +65,8 @@ class QuestionsController < ApplicationController
     private
         def set_question
             @question = Question.find(params[:id])
-            if @question.selected_date.nil?
-                redirect_to questions_path
-            end
+            # if @question.selected_date.nil?
+            #     redirect_to questions_path
+            # end
         end
 end
