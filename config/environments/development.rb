a70627896a90ed6c966443fef4817878579ed1c1
ssm0318 <kleenex0318@gmail.com>
2019-01-18 18:06:37 +0900

    almost done?

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,15 +64,15 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   
-  config.action_mailer.default_url_options = { :host => "adoor.app" }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    user_name:            Rails.application.secrets.gmail_id,
-    password:             Rails.application.secrets.gmail_password,
-    authentication:       'plain',
-    # enable_starttls_auto: true }
-    :openssl_verify_mode  => 'none' }
+  # config.action_mailer.default_url_options = { :host => "adoor.app" }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   user_name:            Rails.application.secrets.gmail_id,
+  #   password:             Rails.application.secrets.gmail_password,
+  #   authentication:       'plain',
+  #   # enable_starttls_auto: true }
+  #   :openssl_verify_mode  => 'none' }
 end
- 
+  
