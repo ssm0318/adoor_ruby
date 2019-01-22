9015e0f522b651df236fa740a64ca946288b2576
leesoojeong <sjlee739@gmail.com>
2019-01-23 00:56:48 +0900

    edit answer in feed

---
+++ app/assets/javascripts/textarea.js
@@ -0,0 +1,47 @@
+// var observe;
+// if (window.attachEvent) {
+//     observe = function (element, event, handler) {
+//         element.attachEvent('on' + event, handler);
+//     };
+// }
+// else {
+//     observe = function (element, event, handler) {
+//         element.addEventListener(event, handler, false);
+//     };
+// }
+
+function observe(element, event, handler) {
+    element.on(event, handler)
+}
+
+function textarea_init (element) {
+    var text = element;
+    function resize () {
+        var scrollLeft = window.pageXOffset ||
+                        (document.documentElement || document.body.parentNode || document.body).scrollLeft;
+
+        var scrollTop  = window.pageYOffset ||
+                        (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+        text.css('height', 'auto')
+        text.css('height', text[0].scrollHeight)
+        // text.style.height = text.scrollHeight + 'px';
+
+        window.scrollTo(scrollLeft, scrollTop);
+    }
+    /* 0-timeout to get the already changed text */
+    function delayedResize () {
+        window.setTimeout(resize, 0);
+    }
+    observe(text, 'change',  resize);
+    observe(text, 'cut',     delayedResize);
+    observe(text, 'paste',   delayedResize);
+    observe(text, 'drop',    delayedResize);
+    observe(text, 'keydown', delayedResize);
+
+
+
+    text.focus();
+    text.select();
+    resize();
+}
