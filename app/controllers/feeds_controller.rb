5d9e681b8d2ceba15c85e79859703d70df58f17c
goojinsun <gjinsun96@gmail.com>
2019-01-23 13:35:09 +0900

    solved posting and custom question

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,7 +2,9 @@ class FeedsController < ApplicationController
     before_action :authenticate_user!
     
     def general
-        @feeds = Answer.channel_name("익명피드").anonymous(current_user.id) + Post.channel_name("익명피드").anonymous(current_user.id)
+        @feeds = Answer.channel_name("익명피드").anonymous(current_user.id) 
+                + Post.channel_name("익명피드").anonymous(current_user.id)
+                + CustomQuestion.channel_name("익명피드").anonymous(current_user.id)
         @feeds = @feeds.sort_by(&:created_at).reverse!
         
         render 'general'
@@ -11,7 +13,8 @@ class FeedsController < ApplicationController
     def friends
         answers = Answer.accessible(current_user.id) + Answer.where(author: current_user)
         posts = Post.accessible(current_user.id) + Post.where(author:current_user)
-        @feeds = answers + posts
+        custom_questions = CustomQuestion.accessible(current_user.id) + CustomQuestion.where(author: current_user)
+        @feeds = answers + posts + custom_questions
         @feeds = @feeds.sort_by(&:created_at).reverse!
 
         render 'friends'
