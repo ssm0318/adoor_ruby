574f94ea9327e98fe85ead59b0e6761ecd2f0f95
ssm0318 <kleenex0318@gmail.com>
2019-01-28 00:24:12 +0900

    mailing

--- app/mailers/stats_mailer.rb
+++ app/mailers/stats_mailer.rb
@@ -5,6 +5,8 @@ class StatsMailer < ApplicationMailer
         attachments["daily_general_stats.csv"] = File.read("lib/user_stats/daily_general_stats.csv")
         attachments["weekly_general_stats.csv"] = File.read("lib/user_stats/weekly_general_stats.csv")
         attachments["monthly_general_stats.csv"] = File.read("lib/user_stats/monthly_general_stats.csv")
+        attachments["total_user_stats.csv"] = File.read("lib/user_stats/total_user_stats.csv")
+        attachments["recent_user_stats.csv"] = File.read("lib/user_stats/recent_user_stats.csv")
         mail(to: "adoor.team@gmail.com", subject: "#{Date.today().strftime("%b. %d, %Y")} 유저 통계입니다.")
     end
 end
