b047d7a985b9a914540a293db0753ff1103db253
ssm0318 <kleenex0318@gmail.com>
2019-01-31 16:59:25 +0900

    temp

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -10,7 +10,7 @@ class NotificationsController < ApplicationController
             n.read_at = DateTime.now()
             n.save(touch: false)
         end
-        redirect_to root_url
+        redirect_to root_url 
     end
 
     def read
