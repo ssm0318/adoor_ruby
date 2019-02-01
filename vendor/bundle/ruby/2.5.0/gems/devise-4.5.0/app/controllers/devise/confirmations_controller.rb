e767ec2aa938ec35164819bf46ea0fff8a1c8bfc
ssm0318 <kleenex0318@gmail.com>
2019-02-02 00:36:38 +0900

    fuck

--- vendor/bundle/ruby/2.5.0/gems/devise-4.5.0/app/controllers/devise/confirmations_controller.rb
+++ vendor/bundle/ruby/2.5.0/gems/devise-4.5.0/app/controllers/devise/confirmations_controller.rb
@@ -12,7 +12,7 @@ class Devise::ConfirmationsController < DeviseController
     yield resource if block_given?
 
     if successfully_sent?(resource)
-      set_flash_message!(:notice, :confirmed)
+      flash[:notice] = "alalalalalalalalalaal"
       respond_with({}, location: after_resending_confirmation_instructions_path_for(resource_name))
     else
       respond_with(resource)
