24cbfd06c8d8bd56b27efddb3979393308005b51
ssm0318 <kleenex0318@gmail.com>
2019-02-02 01:05:41 +0900

    hahaha

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,9 +9,22 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    user = User.find_by_email(params[:email].downcase)
+    if user && user.authenticate(params[:password])
+    if user.email_confirmed
+        sign_in user
+      redirect_back_or user
+    else
+      flash.now[:error] = 'Please activate your account by following the 
+      instructions in the account confirmation email you received to proceed'
+      render 'new'
+    end
+    else
+      flash.now[:error] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
+  end
 
   # DELETE /resource/sign_out
   # def destroy
