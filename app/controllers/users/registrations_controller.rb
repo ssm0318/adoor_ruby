057ef91b6bf729d05b821c1887797ebd019d2e4c
ssm0318 <kleenex0318@gmail.com>
2019-02-01 23:47:42 +0900

    try

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+    flash[:success] = "Please confirm your email address to continue"
+  end
 
   # GET /resource/edit
   # def edit
