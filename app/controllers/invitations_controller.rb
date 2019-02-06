c65bbec5dbd0e328c9a230701220d62f3f1d3702
Yuri <njs03332@gmail.com>
2019-02-06 23:29:39 +0900

    invitation link https

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -13,7 +13,7 @@ class InvitationsController < ApplicationController
         assigned_questions = []   # 선택된 question들을 갖고 있다 (최대 3개, 최소 0개)
         assigned_questions = Question.find(params[:q]) if params[:q]
         if Rails.env.production?
-            @link = "adoor.app/invitations/#{current_user.id}"
+            @link = "https://adoor.app/invitations/#{current_user.id}"
         elsif Rails.env.development?
             @link = "localhost:3000/invitations/#{current_user.id}"
         end
