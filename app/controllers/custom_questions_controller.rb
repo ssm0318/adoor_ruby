79da38a48d7a7f61a4ca266d46f107ba74960737
leesoojeong <sjlee739@gmail.com>
2019-02-08 15:53:09 +0900

    custom question channel edit bug fix

--- app/controllers/custom_questions_controller.rb
+++ app/controllers/custom_questions_controller.rb
@@ -89,71 +89,71 @@ class CustomQuestionsController < ApplicationController
     end
 
     def update
-        if @custom_question.update(custom_question_params)
+        #repost message params가 있는 경우
+        if params[:custom_question]
+            @custom_question.update(repost_message: params[:custom_question][:repost_message])
+        end
 
-            original_channels = @custom_question.channels
-            selected_channels = []
-            selected_channels = Channel.find(params[:c]) if params[:c]
-            # 안겹치는 애들만 모아다가
-            changed_channels = selected_channels - original_channels
-            changed_channels += original_channels - selected_channels
-            # 바꿔줍니다
-            changed_channels.each do |c|
-                e = Entrance.find_or_initialize_by(channel: c, target: @custom_question)
-                e.persisted? ? e.destroy : e.save
+        original_channels = @custom_question.channels
+        selected_channels = []
+        selected_channels = Channel.find(params[:c]) if params[:c]
+        # 안겹치는 애들만 모아다가
+        changed_channels = selected_channels - original_channels
+        changed_channels += original_channels - selected_channels
+        # 바꿔줍니다
+        changed_channels.each do |c|
+            e = Entrance.find_or_initialize_by(channel: c, target: @custom_question)
+            e.persisted? ? e.destroy : e.save
+        end
+
+        ################################## 테스팅 완료
+        # 공개 범위의 변화로 인해 못 보게 된 글 / 댓글에 대한 노티를 모두 삭제한다
+        ## 친구공개였다가 아니게 된 것 : 거기 댓글 / 대댓글 단 친구들에게 간 노티 다 없어짐
+        ## 익명피드 공개였다가 아니게 된 것 : 거기 댓글 / 대댓글 단 사람들한테 간 노티 다 없어짐
+        # visible이고 아니고 다 지우는 걸로 일단 했다
+        ## 즉, 범위를 두 번 바꾼다고 해서 예전 노티가 다시 살아나진 않음
+        comment_join = "INNER JOIN comments ON notifications.origin_id = comments.id AND notifications.origin_type = 'Comment'"
+        reply_join = "INNER JOIN replies ON notifications.origin_id = replies.id AND notifications.origin_type = 'Reply'"
+
+        friend_noties = []
+        friend_noties += Notification.where(target_type: 'Like', action: 'friend_like_comment').joins(comment_join).merge(Comment.where(target: @custom_question)).distinct
+        friend_noties += Notification.where(target_type: 'Like', action: 'friend_like_reply').joins(reply_join).merge(Reply.joins(:comment).where(comments: {target: @custom_question})).distinct
+        friend_noties += Notification.where(target_type: 'Reply', action: 'friend_to_comment').joins(comment_join).merge(Comment.where(target: @custom_question)).distinct
+        friend_noties += Notification.where(target_type: 'Reply', action: 'friend_to_recomment').joins(comment_join).merge(Comment.where(target: @custom_question)).distinct
+
+        friend_noties.each do |n|
+            if (n.recipient != @custom_question.author) && (n.recipient.belonging_channels & selected_channels).empty?
+                n.destroy
             end
-
-            ################################## 테스팅 완료
-            # 공개 범위의 변화로 인해 못 보게 된 글 / 댓글에 대한 노티를 모두 삭제한다
-            ## 친구공개였다가 아니게 된 것 : 거기 댓글 / 대댓글 단 친구들에게 간 노티 다 없어짐
-            ## 익명피드 공개였다가 아니게 된 것 : 거기 댓글 / 대댓글 단 사람들한테 간 노티 다 없어짐
-            # visible이고 아니고 다 지우는 걸로 일단 했다
-            ## 즉, 범위를 두 번 바꾼다고 해서 예전 노티가 다시 살아나진 않음
-            comment_join = "INNER JOIN comments ON notifications.origin_id = comments.id AND notifications.origin_type = 'Comment'"
-            reply_join = "INNER JOIN replies ON notifications.origin_id = replies.id AND notifications.origin_type = 'Reply'"
-
-            friend_noties = []
-            friend_noties += Notification.where(target_type: 'Like', action: 'friend_like_comment').joins(comment_join).merge(Comment.where(target: @custom_question)).distinct
-            friend_noties += Notification.where(target_type: 'Like', action: 'friend_like_reply').joins(reply_join).merge(Reply.joins(:comment).where(comments: {target: @custom_question})).distinct
-            friend_noties += Notification.where(target_type: 'Reply', action: 'friend_to_comment').joins(comment_join).merge(Comment.where(target: @custom_question)).distinct
-            friend_noties += Notification.where(target_type: 'Reply', action: 'friend_to_recomment').joins(comment_join).merge(Comment.where(target: @custom_question)).distinct
-
-            friend_noties.each do |n|
-                if (n.recipient != @custom_question.author) && (n.recipient.belonging_channels & selected_channels).empty?
+        end
+        
+        if original_channels.any?{|c| c.name == '익명피드'} && !selected_channels.any?{|c| c.name == '익명피드'}  # 원래 익명피드 공개였는데 바뀐 경우에만
+            anonymous_noties = []
+            anonymous_noties += Notification.where(target_type: 'Like', action: 'anonymous_like_comment').joins(comment_join).merge(Comment.where(target: @custom_question)).distinct
+            anonymous_noties += Notification.where(target_type: 'Like', action: 'anonymous_like_reply').joins(reply_join).merge(Reply.joins(:comment).where(comments: {target: @custom_question})).distinct
+            anonymous_noties += Notification.where(target_type: 'Reply', action: 'anonymous_to_comment').joins(comment_join).merge(Comment.where(target: @custom_question)).distinct
+            anonymous_noties.each do |n|
+                if n.recipient != @custom_question.author
                     n.destroy
                 end
             end
-            
-            if original_channels.any?{|c| c.name == '익명피드'} && !selected_channels.any?{|c| c.name == '익명피드'}  # 원래 익명피드 공개였는데 바뀐 경우에만
-                anonymous_noties = []
-                anonymous_noties += Notification.where(target_type: 'Like', action: 'anonymous_like_comment').joins(comment_join).merge(Comment.where(target: @custom_question)).distinct
-                anonymous_noties += Notification.where(target_type: 'Like', action: 'anonymous_like_reply').joins(reply_join).merge(Reply.joins(:comment).where(comments: {target: @custom_question})).distinct
-                anonymous_noties += Notification.where(target_type: 'Reply', action: 'anonymous_to_comment').joins(comment_join).merge(Comment.where(target: @custom_question)).distinct
-                anonymous_noties.each do |n|
-                    if n.recipient != @custom_question.author
-                        n.destroy
-                    end
-                end
-            end
-            ########################################
-
-            channel_names = ""
-            selected_channels.each do |c|
-                if c.id != selected_channels.last.id
-                    channel_names += c.name + " | "
-                else
-                    channel_names += c.name
-                end
-            end
-            
-            render :json => {
-                id: @custom_question.id,
-                channels: channel_names,
-                type: "custom-feed"
-            }
-        else
-            redirect_to root_url
         end
+        ########################################
+
+        channel_names = ""
+        selected_channels.each do |c|
+            if c.id != selected_channels.last.id
+                channel_names += c.name + " | "
+            else
+                channel_names += c.name
+            end
+        end
+        
+        render :json => {
+            id: @custom_question.id,
+            channels: channel_names,
+            type: "custom-feed"
+        }
     end
 
     private
