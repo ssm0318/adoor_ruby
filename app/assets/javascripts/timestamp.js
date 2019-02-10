44a909fdf977f3af9963a31efaf4c4cd550160f6
ssm0318 <kleenex0318@gmail.com>
2019-02-10 19:50:37 +0900

    timestamp working with pagination

--- app/assets/javascripts/timestamp.js
+++ app/assets/javascripts/timestamp.js
@@ -1,4 +1,16 @@
-$(document).on('turbolinks:load', function()  {
-    $("time.timeago").timeago();
-    $(".time").css('visibility', 'visible');
-  });
+// $(document).on('turbolinks:load', function()  {
+//     $("time.timeago").timeago();
+//     $(".time").css('visibility', 'visible');
+//   }); 
+
+// $(document).ready(function ()  {
+//   $("time.timeago").timeago();
+//   $(".time").css('visibility', 'visible');
+// }); 
+
+// $(document).on('click', 'button', function() { ... })
+
+$(document).on('click', function() {
+  $("time.timeago").timeago();
+  $(".time").css('visibility', 'visible');
+});
