b505f8c4a3da3b3a6883d543497a405131b35f24
leesoojeong <sjlee739@gmail.com>
2019-02-15 17:11:45 +0900

    general comment show likes bug fix

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -27,13 +27,23 @@ class LikesController < ApplicationController
         users = []
         friends_count = 0
 
-        likes.each do |like|
-            user = like.user
-            if user.id == current_user.id || (current_user.friends.include? user)
+        if params[:target_type] == "Comment" or params[:target_type] == "Reply"
+            #익명댓글 범위에 올린 댓글, 대댓글은 좋아요가 안보이니까 다 실명공개함
+            likes.each do |like|
+                user = like.user
                 users.push({ image_url: user.image.url, profile_path: profile_path(user.slug), username: user.username})
                 friends_count += 1
             end
+        else
+            likes.each do |like|
+                user = like.user
+                if user.id == current_user.id || (current_user.friends.include? user)
+                    users.push({ image_url: user.image.url, profile_path: profile_path(user.slug), username: user.username})
+                    friends_count += 1
+                end
+            end            
         end
+
 
         html_content = render_to_string :partial => 'likes/likes_info', 
             :locals => { :users => users, :friends_count => friends_count, :anonymous_count => likes.count()-friends_count }
